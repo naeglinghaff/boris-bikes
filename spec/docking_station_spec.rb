e3834f737ed7b0e97d19a19fbaf0d78fb622823b
Student <student@Makerss-MacBook-Air.local>
2020-02-18 16:51:17 +0000

    Step 10 - first test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it 'returns bike when bike.working?' do
+    expect(release_bike).to be_working
+  end
 end
